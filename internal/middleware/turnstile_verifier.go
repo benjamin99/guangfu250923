e8474a1705ca6ec805e8c5dcfec732729fac3b81
Benjamin Tsai <cknomber1@gmail.com>
2025-10-05 13:13:20 +0800

    feat(middleware): add the TurnstileVerifier to verify the token

---
+++ internal/middleware/turnstile_verifier.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"net/http"
+	"os"
+
+	"guangfu250923/internal/turnstile"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TurnstileVerifier() gin.HandlerFunc {
+	verifier := turnstile.NewTokenVerifier(os.Getenv("TURNSTILE_SECRET_KEY"))
+
+	return func(c *gin.Context) {
+		token := c.GetHeader("X-ReCaptcha-Token")
+		if token == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "ReCaptcha token is required"})
+			c.Abort()
+			return
+		}
+
+		success, err := verifier.Verify(token)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "failed to verify token" + err.Error()})
+			c.Abort()
+			return
+		}
+
+		if !success {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
